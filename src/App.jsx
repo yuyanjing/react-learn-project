1a1969c20674aabb857ffe04d38c434b3810b406
yuyanjing <yuyanjing@smartlink-tech.com.cn>
2020-08-19 14:50:50 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '技术复核'
+    }
+  }
+  render() {
+    var {name} = this.state;
+    return (
+    <h1>{name}</h1>
+    )
+  }
+}
+
+export default App;
